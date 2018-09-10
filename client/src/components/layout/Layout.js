7718d251fca9bf5399656488d46fc696fab6c705
ninohero <ninohero@muzocalo.com>
2018-09-10 10:51:30 -0400

    committing demo project

---
+++ client/src/components/layout/Layout.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+class Layout extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>MadOwlNews.com -  breaking news about Mad Owls</h1>
+        </div>
+        <div>
+          <ul>
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/about'}>About</Link></li>
+          </ul>
+        </div>
+        <div>
+          { this.props.children }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Layout;
---
+++ client/src/reducers/newsReducer.js
@@ -0,0 +1,23 @@
+import constants from '../constants/actionTypes';
+
+var initialState = {
+  news: [],
+  mnewsItem: {}
+}
+
+export default (state = initialState, action) => {
+  var updated = Object.assign({}, state);
+
+  switch(action.type) {
+    case constants.NEWS_RECEIVED:
+      updated['news'] = action.news;
+      return updated;
+
+    case constants.NEWSITEM_RECEIVED:
+      updated['newsItem'] = action.newsItem;
+      return updated;
+
+    default:
+      return state;
+  }
+}
